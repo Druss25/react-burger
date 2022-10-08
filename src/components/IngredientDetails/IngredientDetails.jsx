c685501d44cad16f1ec9fd651bc411b5833b8ad7
Andrey <druss25@yandex.ru>
2022-10-08 09:26:45 +0500

    Fixit css IngredientDetails and OrderDetails

--- src/components/IngredientDetails/IngredientDetails.jsx
+++ src/components/IngredientDetails/IngredientDetails.jsx
@@ -6,26 +6,26 @@ import IngredientDetailsStyles from './IngredientDetails.module.css'
 const IngredientDetails = (props) => {
 	return (
 		<Modal {...props}>
-			<div style={{ maxWidth: 518, width: '100%', textAlign: 'center' }}>
+			<div className={IngredientDetailsStyles.wrapper__center}>
 				<img src={props.image_large} alt="img_product" />
 			</div>
-			<p className='text text_type_main-medium mt-4 mb-8' style={{ maxWidth: 518, width: '100%', textAlign: 'center', whiteSpace: 'none' }}>{props.name}</p>
-			<div className={`${IngredientDetailsStyles.items__list} mb-15`} style={{ display: 'flex', flexDirection: 'row', alignItems: 'space-between', maxWidth: 518, width: '100%', gap: 20 }}>
-				<div className='text text_type_main-default text_color_inactive' style={{ alignItems: 'center', width: '100%' }}>
-					<p style={{ textAlign: 'center' }}>Калорий,&nbsp;ккалл</p>
-					<p className='text text_type_main-medium mt-2' style={{ textAlign: 'center' }}>{props.calories}</p>
+			<p className={`${IngredientDetailsStyles.wrapper__center} text text_type_main-medium mt-4 mb-8`}>{props.name}</p>
+			<div className={`${IngredientDetailsStyles.items__list} mb-15`}>
+				<div className={`${IngredientDetailsStyles.item} text text_type_main-default text_color_inactive`}>
+					<p className='text-center'>Калорий,&nbsp;ккалл</p>
+					<p className='text text_type_main-medium text-center mt-2' >{props.calories}</p>
 				</div>
-				<div className='text text_type_main-default text_color_inactive' style={{ alignItems: 'center', width: '100%' }}>
-					<p style={{ textAlign: 'center' }}>Белки,&nbsp;г</p>
-					<p className='text text_type_main-medium mt-2' style={{ textAlign: 'center' }}>{props.calories}</p>
+				<div className={`${IngredientDetailsStyles.item} text text_type_main-default text_color_inactive`}>
+					<p className='text-center'>Белки,&nbsp;г</p>
+					<p className='text text_type_main-medium text-center mt-2'>{props.calories}</p>
 				</div>
-				<div className='text text_type_main-default text_color_inactive' style={{ alignItems: 'center', width: '100%' }}>
-					<p style={{ textAlign: 'center' }}>Жиры,&nbsp;г</p>
-					<p className='text text_type_main-medium mt-2' style={{ textAlign: 'center' }}>{props.calories}</p>
+				<div className={`${IngredientDetailsStyles.item} text text_type_main-default text_color_inactive`}>
+					<p className='text-center'>Жиры,&nbsp;г</p>
+					<p className='text text_type_main-medium text-center mt-2'>{props.calories}</p>
 				</div>
-				<div className='text text_type_main-default text_color_inactive' style={{ alignItems: 'center', width: '100%' }}>
-					<p style={{ textAlign: 'center' }}>Углеводы,&nbsp;г</p>
-					<p className='text text_type_main-medium mt-2' style={{ textAlign: 'center' }}>{props.calories}</p>
+				<div className={`${IngredientDetailsStyles.item} text text_type_main-default text_color_inactive`}>
+					<p className='text-center'>Углеводы,&nbsp;г</p>
+					<p className='text text_type_main-medium text-center mt-2'>{props.calories}</p>
 				</div>
 			</div>
 		</Modal>
--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Accept from '../../images/graphics.svg'
+import { modalProps } from '../../utils/data'
 import Modal from '../Modal/Modal'
 // import OrderDetailsStyles from './OrderDetails.module.css'
 
@@ -11,9 +12,13 @@ const OrderDetails = (props) => {
 			<p className='text text_type_main-medium'>идентификатор заказа</p>
 			<img src={Accept} alt="Accept" className='mt-15 mb-15' width={120} height={120} />
 			<p className='text text_type_main-default mb-2'>Ваш заказ начали готовить</p>
-			<p className='text text_type_main-default mb-30' style={{ color: '#8585AD' }}>дождитесь готовности на орбитальной станции</p>
+			<p className='text text_type_main-default text_color_inactive mb-30'>дождитесь готовности на орбитальной станции</p>
 		</Modal>
 	)
+}
+
+OrderDetails.propTypes = {
+	props: modalProps
 }
 
 export default OrderDetails
