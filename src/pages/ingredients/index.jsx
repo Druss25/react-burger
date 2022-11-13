150fccb6037814f42202324383547dae6d8f2cbe
Andrey <druss25@yandex.ru>
2022-11-13 16:49:00 +0500

    Исправил IngredientPage и IngredientDetails ...

--- src/pages/ingredients/index.jsx
+++ src/pages/ingredients/index.jsx
@@ -1,30 +1,12 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import IngredientDetails from '../../components/IngredientDetails/IngredientDetails'
-import { getIngredients } from '../../services/reducers/ingredients/actions'
-import { loadingSelector } from '../../services/reducers/ingredients/selectors'
 
 import styles from './ingredients.module.css'
 
 const IngredientPage = () => {
-  const dispatch = useDispatch()
-  const isLoading = useSelector(loadingSelector)
-
-  React.useEffect(() => {
-    dispatch(getIngredients())
-    // eslint-disable-next-line
-  }, [])
-
-  if (isLoading) {
-    return null
-  }
-
   return (
     <div className={styles.wrapper}>
       <div className={`${styles.content} pt-10 pl-10 pr-10`}>
-        <span className={`${styles.title} text text_type_main-large`}>
-          Детали ингредиента
-        </span>
+        <span className={`${styles.title} text text_type_main-large`}>Детали ингредиента</span>
         <IngredientDetails />
       </div>
     </div>
