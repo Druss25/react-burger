25869ccb84369b2bbdfef59fabd0de631db584d3
Andrey <druss25@yandex.ru>
2022-11-18 12:43:34 +0500

    Улучшил ProtectRoute после ревью...

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Redirect, Route } from 'react-router-dom'
+import { Redirect, Route, RouteProps } from 'react-router-dom'
 import { authSelector } from '../services/reducers/auth/selectors'
 import { getUser } from '../services/reducers/auth/actions'
 import Spinner from '../components/Spinner/Spinner'
 
-interface Props {
-  children: JSX.Element
-  [x: string]: any
-}
+type ProtectRouteTypes = RouteProps & { children?: React.ReactNode }
 
-const ProtectedRoute: React.FC<Props> = ({ children, ...rest }) => {
+const ProtectedRoute: React.FC<ProtectRouteTypes> = ({ children, ...rest }) => {
   const { isAuth, isLoading } = useSelector(authSelector)
   const dispatch = useDispatch()
 
