889b558d15e1dfb47aeef7a69c5c52d4036b45c1
Andrey <druss25@yandex.ru>
2022-11-19 17:10:54 +0500

    Верстаем страницу feed-history...

--- src/components/OrderElement/OrderElement.tsx
+++ src/components/OrderElement/OrderElement.tsx
@@ -13,16 +13,26 @@ const OrderElement: React.FC = () => {
       <h3 className="text text_type_main-medium">Death Star Starship Main бургер</h3>
       <div className={styles.content}>
         {/* -------------   !!! Переписать этот блок   -----------*/}
-        <ul className={styles.image_list}>
-          <li className={styles.image_item}>
+        <div className={styles.image_list}>
+          <div className={styles.image_item}>
+            <p className="text text_type_digits-default">+3</p>
+          </div>
+          <div className={styles.image_item}>
             <img src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" alt="" />
-          </li>
-          <li className={styles.image_item}>2</li>
-          <li className={styles.image_item}>3</li>
-          <li className={styles.image_item}>4</li>
-          <li className={styles.image_item}>5</li>
-          <li className={styles.image_item}>+3</li>
-        </ul>
+          </div>
+          <div className={styles.image_item}>
+            <img src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" alt="" />
+          </div>
+          <div className={styles.image_item}>
+            <img src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" alt="" />
+          </div>
+          <div className={styles.image_item}>
+            <img src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" alt="" />
+          </div>
+          <div className={styles.image_item}>
+            <img src="https://code.s3.yandex.net/react/code/bun-02-mobile.png" alt="" />
+          </div>
+        </div>
         {/* ------------------------------------------------------ */}
         <div className={styles.currency}>
           <span className="text text_type_digits-default ml-6 mr-2">480</span>
