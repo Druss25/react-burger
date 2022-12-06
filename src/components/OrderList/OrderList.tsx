4350868bc773e9a70adda0800f44bc1ccd39d16d
Andrey <druss25@yandex.ru>
2022-12-06 18:06:08 +0500

    Подключение WebSocket

--- src/components/OrderList/OrderList.tsx
+++ src/components/OrderList/OrderList.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { TMessageData } from '../../pages/order-feed'
 import OrderElement from '../OrderElement/OrderElement'
 
 import styles from './OrderList.module.css'
 
-const OrderList: React.FC = () => {
+const OrderList: React.FC<TMessageData> = ({ orders }) => {
+  React.useEffect(() => {
+    return console.log('orders:', orders)
+    //// eslint-disable-next-line
+  }, [orders])
+
   return (
     <div className={`${styles.wrapper} custom-scroll`}>
       <OrderElement />
--- src/pages/order-feed/index.tsx
+++ src/pages/order-feed/index.tsx
@@ -4,13 +4,57 @@ import OrderStatistic from '../../components/OrderStatistic/OrderStatistic'
 
 import styles from './order-feed.module.css'
 
+export type TMessageData = {
+  orders: TOrder[] | null
+  success: Readonly<boolean>
+  total: Readonly<number>
+  totalToday: Readonly<number>
+  event: Readonly<string>
+}
+
+export type TOrder = {
+  _id: Readonly<string>
+  ingredients: Array<string>
+  status: Readonly<string>
+  name: Readonly<string>
+  createdAt: Readonly<string>
+}
+
 const OrderFeedPage: React.FC = () => {
+  const [ingredients, setIngredients] = React.useState<TMessageData>()
+
+  const ws = React.useRef<WebSocket | null>(null)
+  const wsUrl = 'wss://norma.nomoreparties.space/orders/all'
+
+  const onMessage = React.useCallback(() => {
+    if (!ws.current) return
+    ws.current.onmessage = event => {
+      const message = JSON.parse(event.data)
+      if (message.success) {
+        setIngredients(message)
+        console.log(message)
+      }
+    }
+  }, [])
+
+  React.useEffect(() => {
+    ws.current = new WebSocket(wsUrl)
+    ws.current.onopen = () => console.log('Соединение открыто')
+    ws.current.onclose = () => console.log('Соединение закрыто')
+    onMessage()
+    return () => ws.current?.close()
+  }, [ws, onMessage])
+
   return (
     <section className={styles.wrapper}>
       <h3 className="text text_type_main-large mt-10 mb-5">Лента заказов</h3>
       <div className={styles.content}>
-        <OrderList />
-        <OrderStatistic />
+        {typeof ingredients !== 'undefined' && (
+          <>
+            <OrderList {...ingredients} />
+            <OrderStatistic />
+          </>
+        )}
       </div>
     </section>
   )
